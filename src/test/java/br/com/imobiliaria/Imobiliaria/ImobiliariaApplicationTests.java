bab918293c715fdee5e47e8220a943f70c180f09
Amanda Lourenco <amandalourenco67@gmail.com>
2021-01-28 21:08:56 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/imobiliaria/Imobiliaria/ImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.imobiliaria.Imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
